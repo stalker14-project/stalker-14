2ac79faeeb8706a66629a64e52eee1dfc6da31f8
kiriyaga <81517221+Kiriyaga7615@users.noreply.github.com>
2025-01-31 16:11:39 +0100

    arena code cleanup

--- Content.Server/_Stalker/ScreenGrab/ScreenGrabCommand.cs
+++ Content.Server/_Stalker/ScreenGrab/ScreenGrabCommand.cs
@@ -5,33 +5,16 @@ using Content.Shared._Stalker.ScreenGrabEvent;
 
 namespace Content.Server._Stalker.ScreenGrab;
 
+[AdminCommand(AdminFlags.Host)]
 public sealed partial class ScreenGrabCommand : IConsoleCommand
 {
-
-    [Dependency] private readonly IEntityManager _e = default!;
-
     public string Command => "screengrab";
     public string Description => Loc.GetString("screengrab-command-description");
     public string Help => Loc.GetString("screengrab-command-help-text");
 
-
-    [AdminCommand(AdminFlags.Host)]
     public void Execute(IConsoleShell shell, string argstr, string[] args)
     {
-        string target;
-
-        switch (args.Length)
-        {
-            case 1:
-                target = args[0];
-                break;
-            default:
-                shell.WriteError(Loc.GetString("cmd-screengrab-arg-count"));
-                shell.WriteLine(Help);
-                return;
-        }
-
-       // RaiseNetworkEvent(new ScreengrabRequestEvent(), session);
-
+        var entMan = IoCManager.Resolve<IEntityManager>();
+        entMan.EntityNetManager?.SendSystemNetworkMessage(new ScreengrabRequestEvent(), true);
     }
 }
