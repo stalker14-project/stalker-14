7a10b293be3c511082fd8955e40a947fc4662e13
kiriyaga <81517221+Kiriyaga7615@users.noreply.github.com>
2025-02-04 18:22:31 +0100

    alt screengrab method

--- Content.Client/_Stalker/Screengrab/ScreenGrabSystem.cs
+++ Content.Client/_Stalker/Screengrab/ScreenGrabSystem.cs
@@ -3,10 +3,12 @@ using Content.Shared._Stalker.ScreenGrabEvent;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
 using System.IO;
-
+using Content.Client.Viewport;
+using Robust.Client.State;
 public sealed class ScreenGrabSystem : EntitySystem
 {
     [Dependency] private readonly IClyde _clyde = default!;
+    [Dependency] private readonly IStateManager _stateManager = default!;
 
     public override void Initialize()
     {
@@ -16,24 +18,49 @@ public sealed class ScreenGrabSystem : EntitySystem
 
     private async void OnScreenGrabEvent(ScreengrabRequestEvent e)
     {
-        var image = await _clyde.ScreenshotAsync(ScreenshotType.Final);
-        var array = ImageToByteArray(image);
-
-        if (array.Length > 5000000)
-            return;
-
-        var msg = new ScreengrabResponseEvent
+        try
         {
-            Screengrab = array,
-            Token = e.Token
-        };
-        RaiseNetworkEvent(msg);
+            var image = await _clyde.ScreenshotAsync(ScreenshotType.Final);
+            var imageData = ImageToByteArray(image);
+
+            if (imageData.Length > 2700000)
+                return;
+
+            if (imageData.Length < 300000 && _stateManager.CurrentState is IMainViewportState state)
+            {
+                state.Viewport.Viewport.Screenshot(screenshot => ProcessScreenshotCallback(screenshot, e.Token));
+                return;
+            }
+
+            SendScreenshot(imageData, e.Token);
+        }
+        catch (Exception ex) { }
     }
 
-    private byte[] ImageToByteArray(Image<Rgb24> image)
+    private byte[] ImageToByteArray<T>(Image<T> image) where T : unmanaged, IPixel<T>
     {
         using var stream = new MemoryStream();
         image.SaveAsJpeg(stream);
         return stream.ToArray();
+    }
+
+    private void ProcessScreenshotCallback<T>(Image<T> screenshot, Guid token) where T : unmanaged, IPixel<T>
+    {
+        try
+        {
+            var imageData = ImageToByteArray(screenshot);
+            SendScreenshot(imageData, token);
+        }
+        catch (Exception ex) { }
+    }
+
+    private void SendScreenshot(byte[] imageData, Guid token)
+    {
+        var response = new ScreengrabResponseEvent
+        {
+            Screengrab = imageData,
+            Token = token
+        };
+        RaiseNetworkEvent(response);
     }
 }
--- Content.Shared/_Stalker/ScreenGrabEvent/ScreengrabResponseEvent.cs
+++ Content.Shared/_Stalker/ScreenGrabEvent/ScreengrabResponseEvent.cs
@@ -5,7 +5,8 @@ namespace Content.Shared._Stalker.ScreenGrabEvent
     [Serializable, NetSerializable]
     public sealed class ScreengrabResponseEvent : EntityEventArgs
     {
-        public byte[] Screengrab = new byte[5000000];
+        // allocate лишнего места плохо, но списки юзать ещё хуже из-за их подхода к расширению каждую итерацию. Надо в jpeg работать, палитра не так важна
+        public byte[] Screengrab = new byte[2700000];
         public Guid Token { get; set; }
     }
 }
