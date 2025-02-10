5124b2c59ca10917ee673cd7a7b5b516c9ef26e3
DofayS <87414075+DofayS@users.noreply.github.com>
2025-02-11 00:35:54 +0300

    New Music System From Sunrise NMSFS
    spizenno s sanraysa

---
+++ Content.Shared/_Stalker/StalkerCCVars/StalkerCCVars.cs
@@ -0,0 +1,17 @@
+﻿using Robust.Shared.Configuration;
+
+namespace Content.Shared._Stalker.StalkerCCVars;
+
+[CVarDefs]
+public sealed class StalkerCCVars
+{
+    /**
+     * Tape Player
+     */
+
+    /// <summary>
+    /// Параметр отключения школьников с колонками у клиента.
+    /// </summary>
+    public static readonly CVarDef<bool> TapePlayerClientEnabled =
+        CVarDef.Create("tape_player.client_enabled", true, CVar.CLIENTONLY | CVar.ARCHIVE);
+}
---
+++ Content.Shared/_Stalker/TapePlayer/TapePlayerUi.cs
@@ -0,0 +1,10 @@
+using Robust.Shared.Serialization;
+
+namespace Content.Shared._Stalker.TapePlayer;
+
+
+[Serializable, NetSerializable]
+public enum TapePlayerUiKey : byte
+{
+    Key,
+}
