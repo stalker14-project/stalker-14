c44854b649e49f65a7359b247db5e245d4fd86db
CattyLucky <mpalhozaev@gmail.com>
2025-02-21 19:38:54 +0600

    New Code Spawner

---
+++ Content.Server/_Stalker/RandomSpawnerComponent/AdvancedConditionalSpawnerComponent.cs.cs
@@ -0,0 +1,32 @@
+﻿using Robust.Shared.Prototypes;
+
+namespace Content.Server.AdvancedSpawners.Components;
+
+/// <summary>
+/// Улучшенный спавнер, который активируется при определённых игровых условиях.
+/// </summary>
+[RegisterComponent]
+public sealed partial class AdvancedConditionalSpawnerComponent : Component
+{
+    /// <summary>
+    /// Список возможных прототипов, которые могут заспавниться.
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite)]
+    [DataField("prototypes")]
+    public List<string> Prototypes { get; set; } = new();
+
+    /// <summary>
+    /// Список игровых правил, при которых сработает спавн.
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite)]
+    [DataField("gameRules")]
+    public List<string> GameRules { get; set; } = new();
+
+    /// <summary>
+    /// Шанс спавна (значение от 0.0 до 1.0).
+    /// </summary>
+    [ViewVariables(VVAccess.ReadWrite)]
+    [DataField("chance")]
+    public float Chance { get; set; } = 1.0f;
+}
+
---
+++ Content.Server/_Stalker/RandomSpawnerComponent/AdvancedConditionalSpawnerSystem.cs.cs
@@ -0,0 +1,77 @@
+﻿using System.Numerics;
+using Content.Server.GameTicking;
+using Content.Server.AdvancedSpawners.Components;
+using Content.Shared.GameTicking.Components;
+using JetBrains.Annotations;
+using Robust.Shared.Random;
+using Robust.Shared.Map;
+
+namespace Content.Server.AdvancedSpawners.Systems;
+
+/// <summary>
+/// Система, управляющая работой AdvancedConditionalSpawnerComponent.
+/// </summary>
+[UsedImplicitly]
+public sealed class AdvancedConditionalSpawnerSystem : EntitySystem
+{
+    [Dependency] private readonly IRobustRandom _robustRandom = default!;
+    [Dependency] private readonly GameTicker _ticker = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<GameRuleStartedEvent>(OnRuleStarted);
+        SubscribeLocalEvent<AdvancedConditionalSpawnerComponent, MapInitEvent>(OnCondSpawnMapInit);
+    }
+
+    private void OnCondSpawnMapInit(EntityUid uid, AdvancedConditionalSpawnerComponent component, MapInitEvent args)
+    {
+        TrySpawn(uid, component);
+    }
+
+    private void OnRuleStarted(ref GameRuleStartedEvent args)
+    {
+        var query = EntityQueryEnumerator<AdvancedConditionalSpawnerComponent>();
+        while (query.MoveNext(out var uid, out var spawner))
+        {
+            if (spawner.GameRules.Contains(args.RuleId))
+                Spawn(uid, spawner);
+        }
+    }
+
+    private void TrySpawn(EntityUid uid, AdvancedConditionalSpawnerComponent component)
+    {
+        if (component.GameRules.Count == 0)
+        {
+            Spawn(uid, component);
+            return;
+        }
+
+        foreach (var rule in component.GameRules)
+        {
+            if (_ticker.IsGameRuleActive(rule))
+            {
+                Spawn(uid, component);
+                return;
+            }
+        }
+    }
+
+    private void Spawn(EntityUid uid, AdvancedConditionalSpawnerComponent component)
+    {
+        if (component.Chance < 1.0f && !_robustRandom.Prob(component.Chance))
+            return;
+
+        if (component.Prototypes.Count == 0)
+        {
+            Log.Warning($"[AdvancedSpawner] Пустой список прототипов! Объект: {ToPrettyString(uid)}");
+            return;
+        }
+
+        if (!Deleted(uid))
+            EntityManager.SpawnEntity(_robustRandom.Pick(component.Prototypes), Transform(uid).Coordinates);
+    }
+}
+
+
