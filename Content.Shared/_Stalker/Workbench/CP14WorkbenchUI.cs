920e1c33f0a8776ddb9e3e9319480ed3c423144d
CattyLucky <mpalhozaev@gmail.com>
2025-02-26 01:01:55 +0600

    Gotovka

---
+++ Content.Shared/_Stalker/Workbench/CP14WorkbenchUI.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * This file is sublicensed under MIT License
+ * https://github.com/space-wizards/space-station-14/blob/master/LICENSE.TXT
+ */
+
+using Content.Shared._CP14.Workbench.Prototypes;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared._CP14.Workbench;
+
+[Serializable, NetSerializable]
+public enum CP14WorkbenchUiKey
+{
+    Key,
+}
+
+[Serializable, NetSerializable]
+public sealed class CP14WorkbenchUiCraftMessage : BoundUserInterfaceMessage
+{
+    public readonly ProtoId<CP14WorkbenchRecipePrototype> Recipe;
+
+    public CP14WorkbenchUiCraftMessage(ProtoId<CP14WorkbenchRecipePrototype> recipe)
+    {
+        Recipe = recipe;
+    }
+}
+
+
+[Serializable, NetSerializable]
+public sealed class CP14WorkbenchUiRecipesState : BoundUserInterfaceState
+{
+    public readonly List<CP14WorkbenchUiRecipesEntry> Recipes;
+
+    public CP14WorkbenchUiRecipesState(List<CP14WorkbenchUiRecipesEntry> recipes)
+    {
+        Recipes = recipes;
+    }
+}
+
+[Serializable, NetSerializable]
+public readonly struct CP14WorkbenchUiRecipesEntry : IEquatable<CP14WorkbenchUiRecipesEntry>
+{
+    public readonly ProtoId<CP14WorkbenchRecipePrototype> ProtoId;
+    public readonly bool Craftable;
+
+    public CP14WorkbenchUiRecipesEntry(ProtoId<CP14WorkbenchRecipePrototype> protoId, bool craftable)
+    {
+        ProtoId = protoId;
+        Craftable = craftable;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is CP14WorkbenchUiRecipesEntry other && Equals(other);
+    }
+
+    public bool Equals(CP14WorkbenchUiRecipesEntry other)
+    {
+        return ProtoId.Id == other.ProtoId.Id;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(ProtoId, Craftable);
+    }
+}
---
+++ Content.Shared/_Stalker/Workbench/Prototypes/CP14WorkbenchRecipePrototype.cs
@@ -0,0 +1,35 @@
+/*
+ * This file is sublicensed under MIT License
+ * https://github.com/space-wizards/space-station-14/blob/master/LICENSE.TXT
+ */
+
+using Content.Shared.Tag;
+using Robust.Shared.Audio;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._CP14.Workbench.Prototypes;
+
+[Prototype("CP14Recipe")]
+public sealed class CP14WorkbenchRecipePrototype : IPrototype
+{
+    [IdDataField]
+    public string ID { get; private set; } = default!;
+
+    [DataField(required: true)]
+    public ProtoId<TagPrototype> Tag = default!;
+
+    [DataField]
+    public TimeSpan CraftTime = TimeSpan.FromSeconds(1f);
+
+    [DataField]
+    public SoundSpecifier? OverrideCraftSound;
+
+    [DataField(required: true)]
+    public List<CP14WorkbenchCraftRequirement> Requirements = new();
+
+    [DataField(required: true)]
+    public EntProtoId Result;
+
+    [DataField]
+    public int ResultCount = 1;
+}
