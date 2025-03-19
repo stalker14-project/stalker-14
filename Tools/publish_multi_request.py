50f978194df4b593d992bd7786c35c7b37ce97cd
coolmankid12345 <coolmankid123456@gmail.com>
2025-03-19 01:50:05 -0400

    Last time tonight

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -14,7 +14,7 @@ RELEASE_DIR = "release"
 # CONFIGURATION PARAMETERS
 # Forks should change these to publish to their own infrastructure.
 #
-ROBUST_CDN_URL = "http://178.156.140.40:27690/" # TODO: Point stalkers14.xyz to the same IP
+ROBUST_CDN_URL = "http://178.156.140.40:27690/fork/stalker_eng" # TODO: Point stalkers14.xyz to the same IP
 
 def main():
     session = requests.Session()
