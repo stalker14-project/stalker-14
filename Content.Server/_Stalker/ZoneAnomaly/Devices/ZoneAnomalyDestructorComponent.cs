49f6e3c6c744b2edfabbbb3c95e75403028c9ba0
coolmankid12345 <coolmankid123456@gmail.com>
2025-07-04 21:03:37 -0400

    Revert "Scientific equipment now works (almost)"
    This reverts commit 33d23af0

--- Content.Server/_Stalker/ZoneAnomaly/Devices/ZoneAnomalyDestructorComponent.cs
+++ Content.Server/_Stalker/ZoneAnomaly/Devices/ZoneAnomalyDestructorComponent.cs
@@ -1,18 +1,6 @@
-using Robust.Shared.GameStates;
-using Robust.Shared.Serialization;
+﻿namespace Content.Server._Stalker.ZoneAnomaly.Devices;
 
-
-namespace Content.Server._Stalker.ZoneAnomaly.Devices
-
-
+public sealed partial class ZoneAnomalyDestructorComponent : Component
 {
-    [RegisterComponent, NetworkedComponent]
-    public sealed partial class ZoneAnomalyDestructorComponent : Component
-    {
-        [DataField]
-        public float Delay = 20f;
-
-        [DataField]
-        public string TargetPrototype = "ZoneAnomalyBase"; // change to match your actual anomaly prototype ID
-    }
+    
 }
