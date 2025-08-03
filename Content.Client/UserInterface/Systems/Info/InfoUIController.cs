a1b68cb1c4668dbd74494f0a9a61cf1d8fb5f737
TexasPeach517 <xdanxvsx@gmail.com>
2025-08-03 13:40:40 -0400

    Revert "Map Updates!!"
    This reverts commit f5dad416707e69f4ea2be42b92bcd6d866e9a0a4.

--- Content.Client/UserInterface/Systems/Info/InfoUIController.cs
+++ Content.Client/UserInterface/Systems/Info/InfoUIController.cs
@@ -22,7 +22,7 @@ public sealed class InfoUIController : UIController, IOnStateExited<GameplayStat
     private ISawmill _sawmill = default!;
 
     [ValidatePrototypeId<GuideEntryPrototype>]
-    private const string DefaultRuleset = "STRuleset";
+    private const string DefaultRuleset = "DefaultRuleset";
 
     public ProtoId<GuideEntryPrototype> RulesEntryId = DefaultRuleset;
 
